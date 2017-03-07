bb9d3f389702a3308688ff02cdf21e9ccdcfbece
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-07 21:16:29 +0800

    Example3: 加入String @的用法

---
+++ Examples03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sql =
+                @"select * from table
+                where id > 100";
+            string xml =
+                @"<Pokemon>
+                <NationalNo>#001</NationalNo>
+                <Name>Bulbsaur</Name>
+                </Pokemon>";
+            string jason =
+                @"Pokemon ={
+                 NationalNo: '#001',Name:Bulbsaur}";
+            Console.WriteLine(sql);
+            Console.WriteLine(xml);
+            Console.WriteLine(jason);
+        }
+    }
+}
