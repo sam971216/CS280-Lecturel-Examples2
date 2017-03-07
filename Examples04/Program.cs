81de3317506c5f703350b772e326f9d844d3bf02
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-07 21:27:30 +0800

    Example4: 數字轉換 有做錯誤處理

--- Examples04/Program.cs
+++ Examples04/Program.cs
@@ -4,17 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Examples04
+namespace Example04
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("請輸入第一個數字");
-            int number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("請輸入第二個數字");
-            int number2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("{0}+{1}={2}", number1, number2, number1 + number2);
+            int number1 = 0;
+            int number2 = 0;
+            Console.Write("請輸入第一個數字:");
+            try
+            {
+                number1 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.Write("請輸入第二個數字:");
+            try
+            {
+                number2 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("{0} + {1} = {2}", number1, number2, number1 + number2);
         }
     }
 }
